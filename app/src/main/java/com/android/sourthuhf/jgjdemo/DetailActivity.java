77faeca262dcd91313198b64667d9141fb0bf4b1
haomiao.yin <haomiao.yin@esimtek.com>
2020-11-20 15:01:07 +0800

    modify detail

--- app/src/main/java/com/android/sourthuhf/jgjdemo/DetailActivity.java
+++ app/src/main/java/com/android/sourthuhf/jgjdemo/DetailActivity.java
@@ -2,8 +2,12 @@ package com.android.sourthuhf.jgjdemo;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import com.android.sourthuhf.BaseFragment;
 import com.android.sourthuhf.R;
 import com.youth.banner.Banner;
 
@@ -16,14 +20,17 @@ import butterknife.Unbinder;
 
 public class DetailActivity extends AppCompatActivity {
     private Unbinder unBinder;
-    @BindView(R.id.banner)
-    Banner banner;
+    /*@BindView(R.id.banner)
+    Banner banner;*/
+    @BindView(R.id.viewpager)
+    ViewPager mViewpager;
+    private ArrayList<BaseFragment> fragments = new ArrayList<>();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tool_detail);
+        setContentView(R.layout.activity_tool_detail_two);
         unBinder = ButterKnife.bind(this);
-        ArrayList<Integer> images = new ArrayList<>();
+       /* ArrayList<Integer> images = new ArrayList<>();
         images.add(R.mipmap.pic_four);
         images.add(R.mipmap.pic_five);
         banner.setImageLoader(new GlideImageLoader());
@@ -31,7 +38,20 @@ public class DetailActivity extends AppCompatActivity {
         banner.setImages(images);
         banner.isAutoPlay(false);
         //banner设置方法全部调用完毕时最后调用
-        banner.start();
+        banner.start();*/
+        fragments.add(new PictureFragmentOne());
+        fragments.add(new PictureFragmentTwo());
+        mViewpager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return fragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return fragments == null ? 0 : fragments.size();
+            }
+        });
 
     }
     @Override
---
+++ app/src/main/java/com/android/sourthuhf/jgjdemo/PictureFragmentTwo.java
@@ -0,0 +1,16 @@
+package com.android.sourthuhf.jgjdemo;
+
+import com.android.sourthuhf.BaseFragment;
+import com.android.sourthuhf.R;
+
+public class PictureFragmentTwo extends BaseFragment {
+    @Override
+    protected void initEventAndData() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_one;
+    }
+}
